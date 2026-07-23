a26cc9334aeabcc9b3aa9e59a72baf1e024d15e3
odgm <ordyelizkh@gmail.com>
2026-07-23 20:51:34 -0300

    formulario Angular y Bootstrap

--- src/app/agregar-producto/agregar-producto.ts
+++ src/app/agregar-producto/agregar-producto.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-agregar-producto',
   imports: [],
-  templateUrl: './agregar-producto.html',
-  styleUrl: './agregar-producto.css',
+  templateUrl: './agregar-producto.html'
 })
-export class AgregarProducto {}
+export class AgregarProducto {
+  
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductoLista } from './producto-lista/producto-lista';
+import { AgregarProducto } from './agregar-producto/agregar-producto';
 
 // la app responde a la url http://localhost:4200/productos
 export const routes: Routes = [
     { path: 'productos', component: ProductoLista },
-    { path: '', redirectTo: 'productos', pathMatch: 'full' }
+    { path: '', redirectTo: 'productos', pathMatch: 'full' },
+    { path: 'agregar-producto', component: AgregarProducto }
 ];
