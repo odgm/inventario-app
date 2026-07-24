1a4dae0a7e2e5e1b933441004b13fc0bed179d68
odgm <ordyelizkh@gmail.com>
2026-07-24 13:22:57 -0300

    Buscar producto por ID

--- src/app/editar-producto/editar-producto.ts
+++ src/app/editar-producto/editar-producto.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Producto } from '../producto.model';
+import { ProductoService } from '../producto-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar-producto',
   imports: [],
   templateUrl: './editar-producto.html'
 })
-export class EditarProducto {}
+export class EditarProducto {
+  producto: Producto = new Producto();
+  id!: number;
+
+  private productoServicio = inject(ProductoService);
+  private ruta = inject(ActivatedRoute);
+  private cd = inject(ChangeDetectorRef);
+
+  ngOnInit(){
+    this.id = this.ruta.snapshot.params['id'];
+    this.productoServicio.obtenerProductoPorId(this.id).subscribe({
+      next: (datos) => {
+        this.producto = datos
+        this.cd.detectChanges();
+      },
+      error: (errores: any) => console.log(errores)
+    })
+  }
+}
--- src/app/producto-service.ts
+++ src/app/producto-service.ts
@@ -19,4 +19,8 @@ export class ProductoService {
         return this.clienteHttp.post(this.urlBase, producto);
     }
 
+    obtenerProductoPorId(id: number){
+        return this.clienteHttp.get<Producto>(`${this.urlBase}/${id}`);
+    }
+
 }
