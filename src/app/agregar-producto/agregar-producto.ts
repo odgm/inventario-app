785597c8ad5a0c321206cdb086479f9fd292d12d
odgm <ordyelizkh@gmail.com>
2026-07-24 12:09:05 -0300

    Servicio de Agregar

--- src/app/agregar-producto/agregar-producto.ts
+++ src/app/agregar-producto/agregar-producto.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../producto.model';
+import { ProductoService } from '../producto-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './agregar-producto.html'
 })
 export class AgregarProducto {
-  
+  producto: Producto = new Producto;
+
+  private prodcutoServicio = inject(ProductoService);
+  private enrutador = inject(Router);
+
+  onSubmit(){
+    this.guardarProducto();
+  }
+
+  guardarProducto(){
+    this.prodcutoServicio.agregarProducto(this.producto).subscribe({
+      next: (datos) => {
+        this.irListaProductos();
+      }, error: (error: any) => {console.log(error)}
+    });
+  }
+
+  irListaProductos(){
+    this.enrutador.navigate(['/productos']);
+  }
+
 }
